3cf17017cb8cf12019936f81fa3fc0be08cb1cfb
Tefileo Stephens <TStephens@spartaglobal.com>
2020-02-05 16:26:27 +0000

    Push To Cloud

---
+++ Project3.py
@@ -0,0 +1,20 @@
+# List basics :D
+#1 - Define a bucket list with 10 items
+#2 - Print the entire list
+#3 - Check the length of the list / how many items inside the list?
+#4 - print the first item
+#5 - Prin the second item
+#6 - add one more item to the list
+#7 - prin the last item in the list
+#8 - Re assign an item and print all of the list again
+
+bucket_list = ['Get Job', 'Buy House', 'Get Married', 'Have Kids', 'Buy A Sports Car', 'Buy Another House', 'Go On Great Holidays', 'Eat Great Food Everyday', 'Buy An Expensive Watch', 'Hang Out With Friends']
+
+print(bucket_list)
+print(len(bucket_list))
+print(bucket_list[0])
+print(bucket_list[1])
+print(bucket_list.append('Hang Out With Family'))
+print(bucket_list[-1])
+bucket_list[0]='Get A Good Job'
+print(bucket_list)
